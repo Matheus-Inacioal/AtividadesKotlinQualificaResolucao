f0e3d84dea38fb9685a57a28a98c6e5f01eb84f4
Matheus Inácio De Almeida Arruda <81710120+Matheus-Inacioal@users.noreply.github.com>
2023-08-21 15:30:29 -0300

    Initial commit

--- Lista6/atv2.kt
+++ Lista6/atv2.kt
@@ -1,14 +1,18 @@
  //VerificarParImpar
 
-// Função que recebe um número inteiro e verifica se é par ou ímpar
-fun verificarParImpar(numero: Int): String {
-    return if (numero % 2 == 0) {println("par")} else {println("ímpar")}
+// Função que recebe um número inteiro e verifica se ele é par ou ímpar, retornando uma mensagem indicando o resultado
+fun verificarParOuImpar(numero: Int): String {
+    return if (numero % 2 == 0) {
+        "par"
+    } else {
+        "ímpar"
+    }
 }
 
 fun main() {
     println("Digite um número inteiro:")
     val numero = readLine()!!.toInt()
 
-    val resultado = verificarParImpar(numero)
+    val resultado = verificarParOuImpar(numero)
     println("O número $numero é $resultado")
 }
